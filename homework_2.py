2ccd807a19d1ad941e1eac8f66d4c7f94f282d28
Mattew-1 <79008569+Mattew-1@users.noreply.github.com>
2021-03-11 22:00:59 +0300

    Add files via upload

---
+++ homework_2.py
@@ -0,0 +1,9 @@
+def a():
+    ev3.speaker.set_speech_options("en")
+    ev3.speaker.say("melodies №1")
+    ev3.speaker.play_notes("G4/4",'E4/2','E4/4','B4/4','B4/4','B4/4','B4/4','D4/8','D4/4','D4/4','D4/4','E4/4')
+def b(num):
+    ev3.speaker.set_speech_options("en")
+    ev3.speaker.say("melodies №2")
+    for i in range(num)
+        ev3.speaker.play_notes("G4/4","E4/4",'G4/4','B4/4','E4/4','B4/4','G4/4','E4/4','G4/4','B4/4','E4/4','B4/4',"G4/4")
